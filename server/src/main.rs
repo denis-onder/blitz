cc0a9aa2ce0649b0a6379cd01749903c2765aedd
Denis Onder <denis_onder@protonmail.com>
2022-06-11 00:41:34 +0200

    fix: return no posts if reddit API call throws a 404

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,16 +15,19 @@ async fn fetch_feed(source: String, target: String) -> String {
 
     let url = format!("https://www.reddit.com/r/{}.json", target);
     let resp = reqwest::get(&url).await.unwrap();
-    let body = resp.text().await.unwrap();
-
-    let json: serde_json::Value = serde_json::from_str(&body).unwrap();
-    let reddit_posts = json["data"]["children"].as_array().unwrap().to_vec();
 
     let mut posts: Vec<Post> = Vec::new();
 
-    for reddit_post in reddit_posts {
-        let post = translate_reddit_post_to_post(&reddit_post);
-        posts.push(post);
+    if resp.status().is_success() {
+        let body = resp.text().await.unwrap();
+
+        let json: serde_json::Value = serde_json::from_str(&body).unwrap();
+        let reddit_posts = json["data"]["children"].as_array().unwrap().to_vec();
+
+        for reddit_post in reddit_posts {
+            let post = translate_reddit_post_to_post(&reddit_post);
+            posts.push(post);
+        }
     }
 
     let feed = Feed {
